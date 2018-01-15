2fef79f922a1a348aa7d0eb01b1e564e18681403
Matthew Bortolin <matthewbortolin@Matthews-MacBook-Pro.local>
2018-01-15 17:47:44 +1000

    recipe-detail, recipe-list, shopping-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'A test recipe', 'https://www.google.com.au/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwi999Puq9nYAhXH54MKHVtxB1AQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Frecipe-label-icon-symbol-spoon-575434%2F&psig=AOvVaw1d6lgzvpffPx3wl-07jGb_&ust=1516084189671358') 
+  ];
 
   constructor() { }
 
