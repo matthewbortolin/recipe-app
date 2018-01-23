fe3eba952fcd2c9d613f953c6ee8d0bf7eb41fd3
Matthew Bortolin <matthewbortolin@Matthews-MacBook-Pro.local>
2018-01-23 14:46:47 +1000

    ingredients added to recipe model, service and detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,26 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'A test recipe', 
-      'https://c.pxhere.com/images/e8/ff/ab170fd2f2a47e4057318fdbd41b-1418256.jpg!d'),
-    new Recipe('Test Recipe2', 'A test recipe', 
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7IEW8tdiwrWq7kTNfSCNznONYEkYU1GefwdBD8w8CgwTuSpEx') 
+    new Recipe('Test Recipe', 
+      'A test recipe', 
+      'https://c.pxhere.com/images/e8/ff/ab170fd2f2a47e4057318fdbd41b-1418256.jpg!d',
+      [
+        new Ingredient('Prawns', 5),
+        new Ingredient('Rice', 100)
+      ]), 
+    new Recipe('Test Recipe2', 
+      'A test recipe', 
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7IEW8tdiwrWq7kTNfSCNznONYEkYU1GefwdBD8w8CgwTuSpEx',
+      [
+        new Ingredient('Cherry Tomatoes', 5),
+        new Ingredient('Eggplant', 2)
+      ]) 
   ];
 
   getRecipes() {
