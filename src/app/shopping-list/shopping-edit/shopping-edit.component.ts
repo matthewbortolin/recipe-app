c7e68df70f77f521e89446e69620459475818376
Matt <matthewbortolin@hotmail.com>
2018-02-05 22:56:20 +1100

    subscribe to observable to edit item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -9,18 +10,34 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   // inject service
   constructor(private shopListService: ShoppingListService) { }
 
   ngOnInit() {
+    // Subscribe to Subject in shopping list service
+    // when an ingredient is selected
+    this.subscription = this.shopListService.startedEditing.
+      subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+        } 
+      );
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient =  new Ingredient(value.name, value.quantity);
     this.shopListService.addIngredient(newIngredient);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
